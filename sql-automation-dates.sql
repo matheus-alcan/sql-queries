23474e13ad5099406dce682ed77c9654196f871f
Matheus Alcântara <167790351+matheus-alcan@users.noreply.github.com>
2025-09-07 19:09:06 -0300

    Update sql-automation-dates.sql

--- sql-automation-dates.sql
+++ sql-automation-dates.sql
@@ -5,16 +5,17 @@ WHERE
     BETWEEN
     CAST(
         CASE
-            WHEN DATEPART(WEEKDAY, DATEFROMPARTS(YEAR(GETDATE()), MONTH(GETDATE()), DAY(GETDATE()))) IN (2, 3) -- Caso seja D-1 não inclua terça-feira WEEKDAY = 3
-                THEN DATEFROMPARTS(YEAR(DATEADD(DAY, -4, GETDATE())), MONTH(DATEADD(DAY, -4, GETDATE())), 1) -- Caso seja D-1 subtraia 4 dias e pegue o primeiro do mês correspondente
+            WHEN DATEPART(WEEKDAY, DATEFROMPARTS(YEAR(GETDATE()), MONTH(GETDATE()), DAY(GETDATE()))) IN (2, 3) -- Caso seja D-1 nÃ£o inclua terÃ§a-feira WEEKDAY = 3
+                THEN DATEFROMPARTS(YEAR(DATEADD(DAY, -4, GETDATE())), MONTH(DATEADD(DAY, -4, GETDATE())), 1) -- Caso seja D-1 subtraia 3 dias e pegue o primeiro do mÃªs correspondente
             ELSE DATEFROMPARTS(YEAR(GETDATE()), MONTH(GETDATE()), 1)
         END AS DATE
     )
     AND
     CAST(
         CASE
-            WHEN DATEPART(WEEKDAY, GETDATE()) IN (2, 3) -- Caso seja D-1 não inclua terça-feira WEEKDAY = 3
+            WHEN DATEPART(WEEKDAY, GETDATE()) IN (2, 3) -- Caso seja D-1 nÃ£o inclua terÃ§a-feira WEEKDAY = 3
                 THEN DATEADD(DAY, -4, GETDATE()) -- Caso seja D-1 subtraia 4 dias
             ELSE DATEADD(DAY, -2, GETDATE()) -- Caso seja D-1 subtraia 2 dias
         END AS DATE
+
     )
